d5508aa1ecf59566570109464d49526deb55b9ed
Pratyush Sharma <pratyushsharma1404@gmail.com>
2024-08-25 17:26:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     e.preventDefault();
     try {
       const parsedInput = JSON.parse(jsonInput);
-      const res = await fetch('http://localhost:3000/bfhl', {
+      const res = await fetch('https://bajaj-finserv-pratyush-sharma-21-bce-0963.vercel.app/bfhl', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
